691e387fc6053f5b74d344f754078bec96fc6313
gizem <gizembolayir@gmail.com>
2025-04-12 18:45:11 +0300

    edited task3

--- js/task-3.js
+++ js/task-3.js
@@ -5,3 +5,6 @@ function getElementWidth(content, padding, border) {
   const totalWidth = contentValue + 2 * paddingValue + 2 * borderValue;
   console.log(totalWidth);
 }
+getElementWidth("50px", "8px", "4px");       // 74
+getElementWidth("60px", "12px", "8.5px");    // 101
+getElementWidth("200px", "0px", "0px");      // 200
